463a11f398b549b619db41e958f1d500a3a55ce1
Devin <marcelinus.devin@amaan.co.id>
2023-02-28 00:03:11 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.flutter_pokeapi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_pokeapi/app/app.dart';
+import 'package:flutter_pokeapi/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
